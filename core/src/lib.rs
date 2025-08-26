49e4e179bcc7319b0f4dfd269d8f000c97598176
GMN177 <gimmy177@icloud.com>
2025-08-26 17:00:08 +0200

    Initial commit

---
+++ core/src/lib.rs
@@ -0,0 +1,76 @@
+use risc0_zkvm::sha::{Digest, Sha256};
+use serde::{Deserialize, Serialize};
+use std::{fs::File, io::Write};
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct Input {
+    pub image: Vec<u8>,
+    pub hash: Digest,
+}
+
+impl Input {
+    pub fn new(image: &[u8]) -> Self {
+        Self {
+            image: image.to_vec(),
+            hash: *risc0_zkvm::sha::Impl::hash_bytes(&image),
+        }
+    }
+
+    pub fn read_from_file(fname: &str) -> Self {
+        Self::new(&std::fs::read(fname).expect("Could not read file!"))
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct Output {
+    pub image: Vec<u8>,
+    pub hash: Digest,
+}
+
+impl Output {
+    pub fn save_to_file(&self, fname: &str) {
+        let mut file = File::create(fname).expect("Could not create file!");
+
+        file.write_all(&self.image)
+            .expect("Cannot write to the file!");
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Proof {
+    pub receipt: risc0_zkvm::Receipt,
+    pub id: [u32; 8],
+}
+
+impl Proof {
+    pub fn new(receipt: risc0_zkvm::Receipt, id: [u32; 8]) -> Self {
+        Self { receipt, id }
+    }
+
+    pub fn read_from_json(fname: &str) -> Self {
+        let file = File::open(fname).expect("Could not open file!");
+        serde_json::from_reader(file).expect("Could not parse JSON!")
+    }
+
+    pub fn save_as_json(&self, fname: &str) {
+        let proof_as_json = serde_json::to_string(self).expect("Could not serialize to JSON!");
+
+        let mut file = File::create(fname).expect("Could not create file!");
+
+        file.write_all(proof_as_json.as_bytes())
+            .expect("Cannot write to the file!");
+    }
+
+    pub fn verify(&self) {
+        self.receipt.verify(self.id).expect("Verification failed");
+    }
+
+    pub fn print_journal(&self) {
+        let output: Output = self
+            .receipt
+            .journal
+            .decode()
+            .expect("Could not decode journal");
+        println!("Journal output: {:?}", output);
+    }
+}
---
+++ host/src/main.rs
@@ -0,0 +1,74 @@
+use core::{Input, Proof};
+use methods::{METHOD_ELF, METHOD_ID};
+use risc0_zkvm::{default_prover, ExecutorEnv, ProverOpts};
+use std::time::Instant;
+
+const INPUT_FILE: &str = "res/sample_10x10_rgb.tif";
+const PROOF_FILE: &str = "res/proof.json";
+const PROOF_G16_FILE: &str = "res/proof_g16.json";
+
+fn main() {
+    println!("Proving...");
+    let before = Instant::now();
+    prove();
+    println!("Proving time: {:.2?}", before.elapsed());
+
+    println!("Compressing...");
+    let before = Instant::now();
+    compress();
+    println!("Compressing time: {:.2?}", before.elapsed());
+
+    println!("Verifying...");
+    let before = Instant::now();
+    verify();
+    println!("Verifying time: {:.2?}", before.elapsed());
+}
+
+fn prove() {
+    let input = Input::read_from_file(INPUT_FILE);
+
+    let env = ExecutorEnv::builder()
+        .write(&input)
+        .expect("Failed to write input")
+        .build()
+        .expect("Failed to build executor");
+
+    let prover = default_prover();
+
+    let prove_info = prover.prove(env, METHOD_ELF).expect("Proving failed");
+
+    let receipt = prove_info.receipt;
+
+    let proof = Proof::new(receipt, METHOD_ID);
+
+    proof.save_as_json(PROOF_FILE);
+}
+
+fn compress() {
+    let proof = Proof::read_from_json(PROOF_FILE);
+
+    let prover = default_prover();
+
+    let prover_opts = ProverOpts::groth16();
+
+    let compressed_receipt = prover
+        .compress(&prover_opts, &proof.receipt)
+        .expect("Compression failed");
+
+    assert_eq!(
+        compressed_receipt.journal, proof.receipt.journal,
+        "Journal mismatch after decompression"
+    );
+
+    let proof = Proof::new(compressed_receipt, proof.id);
+
+    proof.save_as_json(PROOF_G16_FILE);
+}
+
+fn verify() {
+    let proof = Proof::read_from_json(PROOF_G16_FILE);
+
+    proof.verify();
+
+    proof.print_journal();
+}
